5599e436e7c5f5a0a8bdc5bc8f7f31e3de9a3a17
antonp <grfokpr@gmail.com>
2022-10-31 10:08:19 +0200

    fix : small fix

--- dictionary/exercise_18.py
+++ dictionary/exercise_18.py
@@ -10,11 +10,5 @@ dict_original = {'Theodore': 19, 'Roxanne': 22, 'Mathew': 21, 'Betty': 20}
 maximum = max(dict_original.values())
 minimum = min(dict_original.values())
 
-kk =[]
-
-for x,v in dict_original.items():
-    if v in [minimum, maximum]:
-        kk.append(x)    
-
 keys = tuple([x for x, v in dict_original.items() if v in [minimum, maximum]])
 assert keys[::-1] == ('Roxanne', 'Theodore')
