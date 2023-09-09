c03e8f2e2cfc16916e1c794b78e4aa1e6ed34106
aldazar <alexandreblanchet44@gmail.com>
2023-09-09 17:19:54 +0100

    fix: alex-review

--- 5_oop/models.py
+++ 5_oop/models.py
@@ -4,6 +4,7 @@ class RobotFactory:
     color: str
     type: str
 
+    # TODO: Why do you need to define this a global variable?
     global robot_list
     robot_list = []
 
@@ -14,11 +15,16 @@ class RobotFactory:
         self.type = type
 
     def new_robot(name,identifier,color,type):
+        # TODO: Here you need a different class for RobotFactory and Robot.
+        #   RobotFactory is a way to build Robots in batches
+        #   Robot represents the characteristics and behaviors of one single Robot
         robot = RobotFactory(name,identifier,color,type)
         # robot_list.append([robot.identifier + " - " + robot.name + " (" + robot.type + ") " + "- " + robot.color])
         robot_list.append(robot)
         return robot
 
+    # TODO: By default, Python expect all methods to take self
+    #   as a first parameter
     def robots_created():
         return robot_list
 
@@ -26,6 +32,7 @@ class RobotFactory:
         return message
 
     def walk(self):
+        # TODO: Can you add a docstring?
         if self.type == "fighter":
             message = "I am walking safely"
         else:
@@ -36,6 +43,7 @@ class RobotFactory:
         return message
 
     def fight(self):
+        # TODO: Can you add a docstring?
         if self.type == "fighter":
             message = "Fight in progress ..."
         else:
@@ -44,6 +52,7 @@ class RobotFactory:
         return message
 
     def cook(self):
+        # TODO: Can you add a docstring?
         if self.type == "cook":
             message = "Cooking in progress"
         else:
@@ -52,6 +61,7 @@ class RobotFactory:
         return message
 
     def put_out_fire(self):
+        # TODO: Can you add a docstring?
         if self.type == "firefighter":
             message = "Putting out the fire ..."
         else:
@@ -59,6 +69,10 @@ class RobotFactory:
         
         return message
 
+    # TODO: By default the first parameter of a method is self
+    #   If self is not used inside the method you may want to create a static method
+    #   In our case RobotFactory is used as a "helper", we should not be forced
+    #   to create an instance of RobotFactory ro use the method create_fighter_bots
     def create_fighter_bots(num):
         for i in range(num):
             RobotFactory.new_robot(name="scythe"+str(i), identifier="0000"+str(i), color="metallic", type="fighter")
@@ -102,3 +116,4 @@ class FireFighterRobot(RobotFactory):
 
     def walk(self):
         return super().walk("I am walking fast")
+# TODO: Always go back to the next line, once you finished to write your code
